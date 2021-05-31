345cb6d24bf7950691861fc6435e6ccd9e234a8d
mlopez <lopezmatthew87@gmail.com>
2021-05-31 17:21:42 -0400

    adding logic to button with if/else statement

--- assets/js/modal-init.js
+++ assets/js/modal-init.js
@@ -6,10 +6,28 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var txtEmail = document.getElementById("email").value;
+
+// if (isNaN(email) == "") {
+//     alert = "input is not valid"
+// } else {
+//     alert = "input is ok"
+// };
+
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// if/else statement logic for empty email string
+$(btn).click(function() {
+    if(document.getElementById("email").value.length == 0) {
+        alert("this ish is empty")
+    // modal.style.display = "block";
+    } else {
+        modal.style.display = "block";
+    }
+});  
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
